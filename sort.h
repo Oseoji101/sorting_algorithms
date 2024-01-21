a90f3fe654590a7480ee8e63f67f5a97320851d2
Oseoji101 <nonso.kelvin8770@gmail.com>
2024-01-21 01:18:51 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -18,6 +18,20 @@
 #include <errno.h>
 #include <stddef.h>
 
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
+} listint_t;
+
 void swap_func(int *array, int idx1, int idx2);
 void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);
