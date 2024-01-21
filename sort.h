3d9ae983db1376b2862d30b7673c79aa365d275c
Oseoji101 <nonso.kelvin8770@gmail.com>
2024-01-21 01:08:45 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -2,19 +2,34 @@
 #define SORT_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stddef.h>
 
-void bubble_sort(int *array, size_t size);
-void print_array(const int *array, size_t size);
-void selection_sort(int *array, size_t size);
 void swap_func(int *array, int idx1, int idx2);
 void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void swap_sel(int *array, int idx1, int idx2);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+listint_t *swap_nodes(listint_t **list, listint_t *node1,
+		listint_t *node2, int pos);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
@@ -24,6 +39,4 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quickSorthelper(int *arr, int start, int end, size_t size);
 void quick_sort_hoare(int *arr, int size);
-
-
 #endif
